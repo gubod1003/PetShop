36dde09f12c918a6fcaaf8e7c92e427d8196bf3e
gubod1003 <gubod1003@gmail.com>
2024-05-30 20:14:44 +0300

    add cart

--- src/index.js
+++ src/index.js
@@ -1,44 +1,25 @@
-const buttons = document.querySelectorAll(".store__category-button");
-
-const  changeActiveBtn = (event) => {
-  const target = event.target;
-
-  buttons.forEach((button) => {
-    button.classList.remove('store__category-button_active');
-  });
-  target.classList.add('store__category-button_active');
-};
-
-// const setCategory = (event) => {
-//   const target = event.target;
-
-//   buttons.forEach((button) => {
-//     const category = '';
-//     button.addEventListener('click', () => {
-//       category = target.dataset.category;
-//     });
-//     return category;
-//   });
-// };
-
-buttons.forEach((button) => {
-  button.addEventListener('click', changeActiveBtn);
-});
-
 const API_URL = 'https://intriguing-diagnostic-skink.glitch.me';
 
-const productList = document.querySelector('.store__list');
+const buttons = document.querySelectorAll(".store__category-button");
+const productList = document.querySelector(".store__list");
+const cartButton = document.querySelector(".store__cart-button");
+const cartCount = cartButton.querySelector(".store__cart-cnt");
 
-const createProductCart = (product) => {
+const modalOverlay = document.querySelector(".modal-overlay");
+const cartItemsList = document.querySelector(".modal__cart-items");
+const modalCloseButton = document.querySelector(".modal-overlay_close-button");
+
+
+const createProductCart = ({ photoUrl, name, price }) => {
   const productCart = document.createElement('li');
   productCart.classList.add('store__item');
   productCart.innerHTML = `
     <article class="store__product product">
-      <img class="product__image" src="${API_URL}${product.photoUrl}" alt="${product.name}" width="388" height="261">
+      <img class="product__image" src="${API_URL}${photoUrl}" alt="${name}" width="388" height="261">
 
-      <h3 class="product__title">${product.name}</h3>
+      <h3 class="product__title">${name}</h3>
 
-      <p class="product__price">${product.price}&nbsp;₽</p>
+      <p class="product__price">${price}&nbsp;₽</p>
 
       <button class="product_btn-add-cart btn btn_purple">Заказать</button>
     </article>
@@ -75,4 +56,66 @@ const fetchProductByCategory = async (category) => {
 
 };
 
-fetchProductByCategory("Домики");
+const  changeCategory = ({ target }) => {
+  const category = target.textContent;
+
+  buttons.forEach((button) => {
+    button.classList.remove('store__category-button_active');
+  });
+
+  target.classList.add('store__category-button_active');
+  fetchProductByCategory(category);
+};
+
+buttons.forEach((button) => {
+  button.addEventListener('click', changeCategory);
+
+  if(button.classList.contains('store__category-button_active')) {
+    fetchProductByCategory(button.textContent);
+  }
+});
+
+const renderCartItems = () => {
+  cartItemsList.textContent = '';
+  const cartItems = JSON.parse(localStorage.getItem("cartItems") || "[]");
+
+  cartItems.forEach(item => {
+    const listItem = document.createElement('li');
+    listItem.textContent = item;
+    cartItemsList.append(listItem);
+  })
+};
+
+cartButton.addEventListener('click', () => {
+  modalOverlay.style.display = 'flex';
+  renderCartItems();
+});
+
+modalOverlay.addEventListener('click', ({ target }) => {
+  if (target===modalOverlay || target.closest('.modal-overlay_close-button')) {
+    modalOverlay.style.display = 'none';
+  }
+  
+});
+
+const updateCartCount = () => {
+  const cartItems = JSON.parse(localStorage.getItem("cartItems") || "[]");
+  cartCount.textContent = cartItems.length;
+};
+
+updateCartCount();
+
+const addToCart = (productName) => {
+  const cartItems = JSON.parse(localStorage.getItem("cartItems") || "[]");
+  cartItems.push(productName);
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  updateCartCount();
+};
+
+productList.addEventListener('click', ({ target }) => {
+  if (target.closest('.product_btn-add-cart')) {
+    const productCart = target.closest('.store__product');
+    const productName = productCart.querySelector('.product__title').textContent;
+    addToCart(productName);
+  }
+});
